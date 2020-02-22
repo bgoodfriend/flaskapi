f5362ad9d79d161cdfb176c36b14ea0dfe13f319
Bob Goodfriend <bgoodfriend@localhost.localdomain>
2020-02-22 16:42:49 -0600

    Continue to flesh out query.

--- api.py
+++ api.py
@@ -69,12 +69,6 @@ def query_rate():
     # 2015-07-04T20:00:00+00:00
     start_time = datetime.datetime.strptime( request.args.get('start_time'), "%Y-%m-%dT%H:%M:%S%z" )
     end_time = datetime.datetime.strptime( request.args.get('end_time'), "%Y-%m-%dT%H:%M:%S%z" )
-    #utc_start_time = start_time.astimezone(timezone('UTC'))
-    #utc_end_time = end_time.astimezone(timezone('UTC'))
-    #print(utc_start_time)
-    #print(start_time.strftime('%Z'))
-    #print(start_time.tzname())
-    #print(start_time.weekday())
     print(start_time.isoformat())
     print(end_time.isoformat())
 
@@ -83,12 +77,24 @@ def query_rate():
     #print(start_time.astimezone(timezone('America/Chicago')).weekday())
     #print(start_time.astimezone(timezone('America/Chicago')).hour)
 
-    global rates
     for rate_dict in rates:
         print(rate_dict["tz"])
+        # "7. User input can span more than one day, but the API shouldn't 
+        # "return a valid rate"
+        # Note: Whether a date range spans multiple days can depend on if you
+        # calculate it relative to its submitted tz or the rate bucket's tz.
+        # For purposes of matchign a rate, it makes sense to do the latter
         print(start_time.astimezone(timezone(rate_dict["tz"])).isoformat())
         print(end_time.astimezone(timezone(rate_dict["tz"])).isoformat())
-
+        #if first != second:
+        if start_time.astimezone(timezone(rate_dict["tz"])).weekday() != end_time.astimezone(timezone(rate_dict["tz"])).weekday():
+            print("Days don't match, skipping.")
+            continue;
+        print("Days matched.")
+        print(rate_dict["days"].split(','))
+        for day_of_week in rate_dict["days"].split(','):
+            print(day_of_week)
+        
     return '''<h1>The start time value is: {}</h1>
               <h1>The end time value is:   {}</h1>'''.format(start_time.isoformat(), end_time.isoformat())
 
