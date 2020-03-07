6532d7ebe103da6bcd0224c3f4b1d57330d1c012
Bob Goodfriend <bgoodfriend@localhost.localdomain>
2020-03-07 09:20:35 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flaskapi',
+      version='1.0',
+      description='Flask API proof of concept',
+      url='http://github.com/bgoodfriend/flaskapi',
+      author='Bob Goodfriend',
+      author_email='bob.goodfriend@gmail.com',
+      license='GNU General Public License v3.0',
+      packages=['flaskapi'],
+      zip_safe=False)
